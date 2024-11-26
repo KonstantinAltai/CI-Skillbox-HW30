7df34dbcb3b8ea7a419cba426761b7831325bdf4
KonstantinAltai <69794033+KonstantinAltai@users.noreply.github.com>
2024-11-26 17:27:03 +0700

    add pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,27 @@
+from ..main import client
 
+
+def test_post_dishs():
+    response = client.post(
+        "/recipes",
+        json={
+            "title": "Борщ",
+            "cooking_time": 40,
+            "ingredients": "свекла, картофель, морковь",
+            "recipe": "нарезать, варить"
+        }
+    )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+def test_get_dishs():
+    response = client.get("/recipes", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+def test_get_dish():
+    response = client.get("/recipes/1", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 200
+    assert len(response.json()) > 0
