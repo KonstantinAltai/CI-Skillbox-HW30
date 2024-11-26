20774f097cbc7ed7110613e69872de2136f1daff
KonstantinAltai <69794033+KonstantinAltai@users.noreply.github.com>
2024-11-26 14:14:54 +0700

    commit 1.3

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -15,7 +15,7 @@ client = TestClient(app)
 
 
 @app.on_event("startup")
-async def shutdown():
+async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.create_all)
 
